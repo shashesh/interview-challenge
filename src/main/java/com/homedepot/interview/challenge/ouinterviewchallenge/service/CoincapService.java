cfe71b28b489537563f0cb294d8c5136942360fb
Shashesh Silwal <shashesh@outlook.com>
2025-02-28 12:19:05 -0500

    Making changes to the interview challenge

--- src/main/java/com/homedepot/interview/challenge/ouinterviewchallenge/service/CoincapService.java
+++ src/main/java/com/homedepot/interview/challenge/ouinterviewchallenge/service/CoincapService.java
@@ -1,33 +1,17 @@
 package com.homedepot.interview.challenge.ouinterviewchallenge.service;
 
 import com.homedepot.interview.challenge.ouinterviewchallenge.entity.CoincapAsset;
-import com.homedepot.interview.challenge.ouinterviewchallenge.entity.CoincapAssetWrapper;
 import com.homedepot.interview.challenge.ouinterviewchallenge.entity.CryptoExchangeRate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.NoSuchElementException;
 
 
 @Service
 public class CoincapService {
 
-    private final RestTemplate coincapRestTemplate;
-
-    private final String coincapBaseUrl;
-    private final String coincapToken;
-
-    public CoincapService(RestTemplate restTemplate,
-                            @Value("${coincap.api.url}") String coincapBaseUrl,
-                            @Value("${coincap.api.token}") String coincapToken) {
-        this.coincapBaseUrl = coincapBaseUrl;
-        this.coincapToken = coincapToken;
-        this.coincapRestTemplate = restTemplate;
+    public CoincapService() {
     }
 
 
@@ -43,24 +27,11 @@ public class CoincapService {
      * Searches the Coincap API for a token by Symbol name
      * @param tokenSymbol
      * @return a Coincap Asset
-     * @throws NoSuchElementException
      */
-    private CoincapAsset findTokenFromSymbol(String tokenSymbol) {
+    private CoincapAsset findTokenFromSymbolUpstreamCoincapAPI(String tokenSymbol) {
 
-        String assetSearchUrl = "/v2/assets?search=" + tokenSymbol;
-        CoincapAssetWrapper returnValue = coincapRestTemplate
-            .exchange(coincapBaseUrl + assetSearchUrl,
-                HttpMethod.GET,
-                getCoincapHeaders(),
-                CoincapAssetWrapper.class)
-            .getBody();
-
-        return returnValue
-            .getData()
-            .stream()
-            .filter(a -> a.getSymbol().equalsIgnoreCase(tokenSymbol))
-            .findAny()
-            .orElseThrow();
+        // TODO: Implement Coincap API service call
+        return null;
     }
 
     /**
@@ -70,7 +41,8 @@ public class CoincapService {
     private HttpEntity<HttpHeaders> getCoincapHeaders() {
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + coincapToken);
+        // TODO: Need to implement logic to pass Authorization token
+
         headers.add("Accept-Encoding", "deflate");
         headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
         return new HttpEntity<>(headers);
